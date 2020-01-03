76b123e4de693dc7a5136694548c368cab8161c8
Millitar <59485168+Millitar@users.noreply.github.com>
2020-01-03 21:25:08 +0100

    Update main.c

--- main.c
+++ main.c
@@ -28,7 +28,7 @@ void liczby_od1_do10()
 //Adam
 void wysw_imienia()
 {
-    system("clear");//konflikt ci zrobiłam
+    system("clear");
     char user [100];
     scanf("%s", &user);
     printf("%s", user);
